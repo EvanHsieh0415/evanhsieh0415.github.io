ada57c5da0c36fda3b68252569e7d116b01049a9
Mango <evan940415@gmail.com>
2024-06-18 08:05:36 +0800

    add games/

--- docs/.vuepress/definitions/plugins.ts
+++ docs/.vuepress/definitions/plugins.ts
@@ -1,7 +1,13 @@
 import { markdownContainerPlugin } from "@vuepress/plugin-markdown-container";
+import { componentsPlugin } from "vuepress-plugin-components";
 
 export const plugins = [
   markdownContainerPlugin({
     type: "tip",
   }),
+  componentsPlugin({
+    components: [
+      "VPCard"
+    ],
+  })
 ];
